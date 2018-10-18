0bdd8544beb1cbd3871900e5c1d6b78cec55655e
natalyVerdugoNogue <nataly.verdugo.nogue@gmail.com>
2018-10-17 23:30:04 -0300

    6 proyectos

--- src/js/view.js
+++ src/js/view.js
@@ -64,7 +64,7 @@ window.view.proyect01 = () => {
       <div class="row m-0  fondo-proyectos">
         <div class="col-10">
           <div class="row m-0">
-            <div class="col-12 pt-3">
+            <div class="col-12 pt-3 alt-xl">
               <h1 class="title-proy text-center">Cipher Cesar</h1>
             </div>
             <div class="col-12 col-md-6">
@@ -75,10 +75,10 @@ window.view.proyect01 = () => {
                   Para el desarrollo se utilizó JavaScript, HTML y CSS.
                 </p>
                 <p class="descr-proy text-justify px-3 pt-3">
-                  <a href="https://github.com/NatalyVerdugoNogue/scl-2018-01-foodmap" target="_blank">Git Hub</a>
+                  <a href="https://github.com/NatalyVerdugoNogue/scl-2018-05-bc-core-am" target="_blank">Git Hub</a>
                 </p>
                 <p class="descr-proy text-justify px-3 pt-3">
-                  <a href="https://natalyverdugonogue.github.io/scl-2018-01-foodmap/" target="_blank">App Deploy</a>
+                  <a href="https://natalyverdugonogue.github.io/scl-2018-05-bc-core-am/" target="_blank">App Deploy</a>
                 </p>
             </div>
             <div class="col-12 col-md-6">
@@ -103,6 +103,9 @@ window.view.proyect01 = () => {
             <li class="nav-item py-2">
               <a class="nav-link" onclick="window.view.proyect05()">05</a>
             </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect06()">06</a>
+            </li>
             <li class="py-2">
               <button class="col-8 btn-back-proy p-2" onclick="window.controller.cover()">
                 <span class="vis-md text-back">Regresar</span>
@@ -122,20 +125,25 @@ window.view.proyect02 = () => {
       <div class="row m-0  fondo-proyectos">
         <div class="col-10">
           <div class="row m-0">
-            <div class="col-12 pt-3">
+            <div class="col-12 pt-3 alt-xl">
               <h1 class="title-proy text-center">Food Map</h1>
             </div>
             <div class="col-12 col-md-6">
               <img src="src/img/opcFoodMap.png" alt="Proyecto-FoodMap" class="img-fluid">
             </div>
             <div class="col-12 col-md-6">
-              <p class="descr-proy text-justify px-3 pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Delectus
-                perferendis nihil
-                commodi cum
-                laudantium fugiat. Rem dolore, explicabo, inventore, placeat accusamus impedit praesentium architecto
-                tenetur nesciunt consectetur repudiandae. Delectus, vero.
-                </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+              Web-App que utiliza google-map API, en la que se puede filtrar restaurantes cercanos y mostrar información sobre ellos a través de un modal.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+              Desarrollado completamente en JavaScript, con consideraciones de diseño generales determinadas por <a href="https://github.com/NatalyVerdugoNogue/scl-2018-01-foodmap/blob/master/ReadmeLaboratoria.md" target="_blank">Laboratoria</a>, quien planteo el proyecto.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+                <a href="https://github.com/NatalyVerdugoNogue/scl-2018-01-foodmap" target="_blank">Git Hub</a>
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+                <a href="https://natalyverdugonogue.github.io/scl-2018-01-foodmap/" target="_blank">App Deploy</a>
+              </p>
             </div>
           </div>
         </div>
@@ -156,6 +164,9 @@ window.view.proyect02 = () => {
             <li class="nav-item py-2">
               <a class="nav-link" onclick="window.view.proyect05()">05</a>
             </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect06()">06</a>
+            </li>
             <li class="py-2">
               <button class="col-8 btn-back-proy p-2" onclick="window.controller.cover()">
                 <span class="vis-md text-back">Regresar</span>
@@ -169,33 +180,157 @@ window.view.proyect02 = () => {
 };
 
 window.view.proyect03 = () => {
-  let divProyect02 = document.getElementById('container');
-  divProyect02.innerHTML =
-    `<div class="row m-0 row-container">
-    <div class="col-12 col-md-10 m-0 m-md-auto p-0 img-nav01">
-      <div class="row m-0 row-img-nav">
-        <div class="col-10 p-0 pt-2">
+  let divProyect03 = document.getElementById('container');
+  divProyect03.innerHTML =
+    `<div class="container p-0 my-md-5">
+      <div class="row m-0  fondo-proyectos">
+        <div class="col-10">
           <div class="row m-0">
-            <div class="col-12 pt-2">
-              <h1 class="title-proy text-center">Pinterest</h1>
-            </div>
-            <div class="col-12 col-md-6 p-3">
-              <p class="descr-proy text-justify px-3 pt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Delectus
-                perferendis nihil
-                commodi cum
-                laudantium fugiat. Rem dolore, explicabo, inventore, placeat accusamus impedit praesentium architecto
-                tenetur nesciunt consectetur repudiandae. Delectus, vero.</p>
+            <div class="col-12 pt-3 alt-xl">
+              <h1 class="title-proy text-center">Markdown Links</h1>
             </div>
             <div class="col-12 col-md-6">
-              <img src="src/img/opcPinterest.png" alt="Poryecto-Pinterest" class="img-fluid">
+              <p class="descr-proy text-justify px-3 pt-3">
+                  Librería en JavaScript que desde un archivo markdown (.md), si este contiene enlaces, según lo requerido, mostrara los enlaces, la linea y el texto de todos los enlaces en el archivo.
+                </p>
+                <p class="descr-proy text-justify px-3 pt-3">
+                  Desarrollado en Node.js y testeado con Jest.js.
+                </p>
+                <p class="descr-proy text-justify px-3 pt-3">
+                  <a href="https://github.com/NatalyVerdugoNogue/scl-2018-01-FE-markdown" target="_blank">Git Hub</a>
+                </p>
+                <p class="descr-proy text-justify px-3 pt-3">
+                  <a href="https://www.npmjs.com/package/md-links-nvn" target="_blank">npm</a>
+                </p>
+            </div>
+            <div class="col-12 col-md-6">
+              <img src="src/img/opcCipherCesar.png" alt="Proyecto-MarkdownLinks" class="img-fluid">
             </div>
           </div>
         </div>
-        <div class="col-2 p-0 nav-tot text-center tabla">
-          <ul class="nav flex-column py-4">
+        <div class="col-2 fondo-slice nav-tot p-0">
+          <ul class="nav flex-column py-4 list-tot">
             <li class="nav-item py-2">
-              <a class="nav-link active" onclick="window.view.proyect01()">01</a>
+              <a class="nav-link" onclick="window.view.proyect01()">01</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect02()">02</a>
+            </li>
+            <li class="nav-item py-2 here">
+              <a class="nav-link active" onclick="window.view.proyect03()">03</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect04()">04</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect05()">05</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect06()">06</a>
+            </li>
+            <li class="py-2">
+              <button class="col-8 btn-back-proy p-2" onclick="window.controller.cover()">
+                <span class="vis-md text-back">Regresar</span>
+                <i class="fas fa-undo icon-back vis-xl"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+};
+
+window.view.proyect04 = () => {
+  let divProyect04 = document.getElementById('container');
+  divProyect04.innerHTML =
+    `<div class="container p-0 my-md-5">
+      <div class="row m-0  fondo-proyectos">
+        <div class="col-10">
+          <div class="row m-0">
+            <div class="col-12 pt-3 alt-xl">
+              <h1 class="title-proy text-center">Easy Register</h1>
+            </div>
+            <div class="col-12 col-md-6">
+              <img src="src/img/opcEasyRegister.png" alt="Proyecto-EasyRegister" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-6">
+              <p class="descr-proy text-justify px-3 pt-3">
+              Web-App para realizar el registro de visitantes en un complejo de cowork o residencial y facilitar el trabajo de conserjería.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+              Desarrollado en JavaScript, HTML y CSS; utilizando Firebase como base de datos Cloud.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+                <a href="https://github.com/NatalyVerdugoNogue/scl-2018-01-ProyectoFinalCore" target="_blank">Git Hub</a>
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+                <a href="https://finalcorelaboratoria.firebaseapp.com/" target="_blank">App Deploy</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 fondo-slice nav-tot p-0">
+          <ul class="nav flex-column py-4 list-tot">
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect01()">01</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect02()">02</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect03()">03</a>
+            </li>
+            <li class="nav-item py-2  here">
+              <a class="nav-link active" onclick="window.view.proyect04()">04</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect05()">05</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect06()">06</a>
+            </li>
+            <li class="py-2">
+              <button class="col-8 btn-back-proy p-2" onclick="window.controller.cover()">
+                <span class="vis-md text-back">Regresar</span>
+                <i class="fas fa-undo icon-back vis-xl"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+};
+
+window.view.proyect05 = () => {
+  let divProyect05 = document.getElementById('container');
+  divProyect05.innerHTML =
+    `<div class="container p-0 my-md-5">
+      <div class="row m-0  fondo-proyectos">
+        <div class="col-10">
+          <div class="row m-0">
+            <div class="col-12 pt-3 alt-xl">
+              <h1 class="title-proy text-center">Memory Cards</h1>
+            </div>
+            <div class="col-12 col-md-6">
+              <p class="descr-proy text-justify px-3 pt-3">
+                  Juego de memoria de cartas que muestra 8 cartas sobre la mesa, aleatorias, todas con pareja, una vez encontrada su pareja desaparecen.
+                </p>
+                <p class="descr-proy text-justify px-3 pt-3">
+                  Desarrollado completamente en Angular y utilizando Bootstrap 4.
+                </p>
+                <p class="descr-proy text-justify px-3 pt-3">
+                  <a href="https://github.com/NatalyVerdugoNogue/desafioLaboratoria2" target="_blank">Git Hub</a>
+                </p>
+            </div>
+            <div class="col-12 col-md-6">
+              <img src="src/img/opcCipherCesar.png" alt="Proyecto-MemoryCards" class="img-fluid">
+            </div>
+          </div>
+        </div>
+        <div class="col-2 fondo-slice nav-tot p-0">
+          <ul class="nav flex-column py-4 list-tot">
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect01()">01</a>
             </li>
             <li class="nav-item py-2">
               <a class="nav-link" onclick="window.view.proyect02()">02</a>
@@ -204,28 +339,16 @@ window.view.proyect03 = () => {
               <a class="nav-link" onclick="window.view.proyect03()">03</a>
             </li>
             <li class="nav-item py-2">
-              <a class="nav-link" href="#">04</a>
+              <a class="nav-link" onclick="window.view.proyect04()">04</a>
+            </li>
+            <li class="nav-item py-2  here">
+              <a class="nav-link active" onclick="window.view.proyect05()">05</a>
             </li>
             <li class="nav-item py-2">
-              <a class="nav-link" href="#">05</a>
-            </li>
-            <li class="nav-item py-2">
-              <a class="nav-link" href="#">06</a>
-            </li>
-            <li class="nav-item py-2">
-              <a class="nav-link" href="#">07</a>
-            </li>
-            <li class="nav-item py-2">
-              <a class="nav-link" href="#">08</a>
-            </li>
-            <li class="nav-item py-2">
-              <a class="nav-link" href="#">09</a>
-            </li>
-            <li class="nav-item py-2">
-              <a class="nav-link" href="#">10</a>
+              <a class="nav-link" onclick="window.view.proyect06()">06</a>
             </li>
             <li class="py-2">
-              <button class="col-8 btn-back-proy p-0" onclick="window.controller.cover()">
+              <button class="col-8 btn-back-proy p-2" onclick="window.controller.cover()">
                 <span class="vis-md text-back">Regresar</span>
                 <i class="fas fa-undo icon-back vis-xl"></i>
               </button>
@@ -233,6 +356,69 @@ window.view.proyect03 = () => {
           </ul>
         </div>
       </div>
-    </div>
-  </div>`;
+    </div>`;
+};
+
+window.view.proyect06 = () => {
+  let divProyect06 = document.getElementById('container');
+  divProyect06.innerHTML =
+    `<div class="container p-0 my-md-5">
+      <div class="row m-0  fondo-proyectos">
+        <div class="col-10">
+          <div class="row m-0">
+            <div class="col-12 pt-3 alt-xl">
+              <h1 class="title-proy text-center">Pinterest</h1>
+            </div>
+            <div class="col-12 col-md-6">
+              <img src="src/img/opcPinterest.png" alt="Proyecto-Pinterest" class="img-fluid">
+            </div>
+            <div class="col-12 col-md-6">
+              <p class="descr-proy text-justify px-3 pt-3">
+              Replica de Pinteres realizada paralelamente en JavaScript y Angular, ambos utilizando Bootstrap 4. En el que se consulta una API de imágenes que son cargadas siguiendo la estructura de Pinterest.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+              El modelo desarrollado en JavaScript es responsive, y ademas se puede realizar una búsqueda de imágenes por tema.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+                <a href="https://github.com/NatalyVerdugoNogue/pinterest" target="_blank">Git Hub</a>
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+              El modelo desarrollado en angular tiene una barra de palabras claves con colores al azar, y al cargar 20 imágenes y hacer scroll al final cargan 20 imágenes mas.
+              </p>
+              <p class="descr-proy text-justify px-3 pt-3">
+                <a href="https://github.com/NatalyVerdugoNogue/desafioLaboratoria6" target="_blank">Git Hub</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-2 fondo-slice nav-tot p-0">
+          <ul class="nav flex-column py-4 list-tot">
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect01()">01</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect02()">02</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect03()">03</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect04()">04</a>
+            </li>
+            <li class="nav-item py-2">
+              <a class="nav-link" onclick="window.view.proyect05()">05</a>
+            </li>
+            <li class="nav-item py-2 here">
+              <a class="nav-link active" onclick="window.view.proyect06()">06</a>
+            </li>
+            <li class="py-2">
+              <button class="col-8 btn-back-proy p-2" onclick="window.controller.cover()">
+                <span class="vis-md text-back">Regresar</span>
+                <i class="fas fa-undo icon-back vis-xl"></i>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
 };
